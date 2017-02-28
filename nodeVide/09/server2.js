ea8f69afd545da903136f62157872fd13a7df72c
yang-chris <18813068824@163.com>
2017-02-28 15:48:50 +0800

    express实现简单的登陆注册

--- nodeVide/09/server2.js
+++ nodeVide/09/server2.js
@@ -12,12 +12,16 @@ var users={
     'qwe':'qwe',
     '123':'123'
 };
-console.log(users.blue);
+
+//console.log(users.blue);
 server.listen(8080);
 server.get('/login',function (req,res) {
     var user=req.query['user'];
     var pass=req.query['pass'];
-    console.log(user,pass);
+    console.log("1"+user);
+    console.log("2"+pass);
+
+    //验证登陆
     if(users[user]==null){
         res.send({ok:false,msg:'此用户不存在'});
     }else{
@@ -26,6 +30,19 @@ server.get('/login',function (req,res) {
         }else{
             res.send({ok:true,msg:'登录成功'})
         }
+    }
+});
+server.get('/reg',function (req,res) {
+    var regUser=req.query['user'];
+    var regPass=req.query['pass'];
+
+    //验证注册
+    if(users[regUser]){
+        res.send({ok:false,msg:'此用户名已存在，请重新注册'})
+    }else{
+        res.send({ok:true,msg:'注册成功'});
+        users[regUser]=regPass;
+        console.log(users);
     }
 });
 
